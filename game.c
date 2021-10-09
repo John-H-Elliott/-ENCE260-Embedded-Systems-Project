6dd0040aac5a4ba664c9e88e42e5230c386c8e10
Menghao Zhan <mzh99@uclive.ac.nz>
2021-10-10 11:04:50 +1300

    Made title on gmae.c with author name ...

---
+++ game.c
@@ -0,0 +1,22 @@
+/** @file   game.c
+    @author ZHAN (mzh99)      ELLIOTT (jel119)
+    @date   20 October 2021
+    @brief  --
+
+    @defgroup --
+*/
+
+#include "system.h"
+
+int main (void)
+{
+    system_init ();
+
+
+    while (1)
+    {
+
+
+
+    }
+}
