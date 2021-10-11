b859deaf6bf514b899adbcdc7b8d475f9f83dd65
Menghao Zhan <mzh99@uclive.ac.nz>
2021-10-11 20:02:54 +1300

    Upload some include files

--- game.c
+++ game.c
@@ -5,8 +5,41 @@
 
     @defgroup --
 */
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "system.h"
+#include "led.h"
+#include "button.h"
+#include "navswitch.h"
+#include "pacer.h"
+#include "tinygl.h"
+#include "../../fonts/font5x7_1.h"
+#include "dodge_dash.h"
+
+
+/* Define game statements used in the game */
+typedef enum Game_state_e
+{
+    GAME_START,
+    GAME_WIN,
+    GAME_LOSS,
+    GAME_RESET
+} Game_state_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main (void)
 {
